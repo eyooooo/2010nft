fb763b5200b9b478a11264c13d655a76cd8a9879
eyooooo <eyooo@seriousbusiness.xyz>
2021-08-27 14:48:57 -0400

    asdf

--- makesvg.js
+++ makesvg.js
@@ -9,7 +9,7 @@ function main() {
 		    	<style>
 	            	.Rrrrr { font: italic 48px serif; fill: red; }
 	          	</style>
-		        <text x="0" y="25" class="Rrrrr">${i}</text>
+		        <text x="10" y="25" class="Rrrrr">${i}</text>
 		    </svg>
 		    `
 
