bfc53a0c75ce7cf628c8d6d1f5e80b0d8bb6f085
eyooooo <eyooo@seriousbusiness.xyz>
2021-08-27 15:01:41 -0400

    asdf

--- makesvg.js
+++ makesvg.js
@@ -5,9 +5,9 @@ const fs = require('fs')
 function main() {
 	for (let i=0; i < 2010; i++) {
 		var data = `
-	        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+	        <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
 		    	<style>
-	            	.Rrrrr { font: italic 32px serif; fill: red; }
+	            	.Rrrrr { font: italic 24px serif; fill: red; }
 	          	</style>
 		        <text x="0" y="75" class="Rrrrr">${i}</text>
 		    </svg>
