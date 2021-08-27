430884ca24befe419ced0514c61f8e1f61291b95
eyooooo <eyooo@seriousbusiness.xyz>
2021-08-27 14:20:15 -0400

    add stuff

---
+++ makesvg.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+
+
+function main() {
+	for (let i=0; i < 2010; i++) {
+		var data = `
+	        <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+		                  <style>
+		                    .small { font: italic 13px sans-serif; }
+	            .heavy { font: bold 30px sans-serif; }
+
+	            .Rrrrr { font: italic 32px serif; fill: red; }
+	          </style>
+
+		                  <text x="50" y="40" class="Rrrrr">${i}</text>
+		                </svg>
+		                `
+
+		const path = `./${i}.svg`
+		console.log(path)
+		fs.writeFile(path, data, err => {
+			if (err) {
+				console.error(err)
+				return
+			}
+		})
+	}
+}
+main()
