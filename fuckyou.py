11dac43d69b4130cb616457672a01c7598506a37
eyooooo <eyooo@seriousbusiness.xyz>
2021-08-27 16:10:56 -0400

    asdf

--- fuckyou.py
+++ fuckyou.py
@@ -4,7 +4,7 @@ from PIL import ImageFont
 
 img = Image.new('RGB', (50, 50))
 d = ImageDraw.Draw(img)
-font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 28)
+font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 24)
 d.text((0, 20), '1999', fill=(255, 0, 0), font=font)
 
 img.save('./0.png')
